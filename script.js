e25ab23fbf1554f02c416085114a10dadf4d8f93
ChitteGnanaPrasannKumarReddy <116110463+ChitteGnanaPrasannKumarReddy@users.noreply.github.com>
2024-10-22 17:28:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-function makeid(l) {
+function makeid(l) { 
+	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    
+    for (let i = 0; i < l; i++) {
+        
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    
+    return result;
+
   // write your code here
 }
 
